1913f4e59df5f9246d6ddf06e202ac6de6991cf5
rajivking6 <rajiv@ericavijay.net>
2017-05-24 08:32:33 -0400

    image canvas added

---
+++ percolation/src/ImageCanvas.java
@@ -0,0 +1,36 @@
+import java.awt.Canvas;
+import java.awt.Dimension;
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+public class ImageCanvas extends Canvas {
+
+        private BufferedImage img;
+
+        public ImageCanvas() {
+            try {
+                img = ImageIO.read(new File("C:/Users/Rajiv Sarvepalli/Desktop/8puzzle1.jpg"));  //put in the location of 8puzzle1.jpg, for example C:/Users/Rajiv Sarvepalli/Desktop/School work/AP CS/8puzzle1.jpg
+            } catch (IOException ex) {
+                ex.printStackTrace();
+				}
+		  }
+	 	  public Dimension getPreferredSize() {
+            return img == null ? new Dimension(200, 200) : new Dimension(img.getWidth(), img.getHeight());
+        }        
+		  @Override
+        public void paint(Graphics g) {
+            super.paint(g);
+            if (img != null) {
+                int x = (getWidth() - img.getWidth()) / 2;
+                int y = (getHeight() - img.getHeight()) / 2;
+                g.drawImage(img, x, y, this);
+            }
+        }
+
+    }
